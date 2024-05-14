8fdfdbf4249a9765126a92f51ca3ebaae230b728
MukhammadAli <133798412+JavaDevALi@users.noreply.github.com>
2024-05-14 11:02:15 +0500

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import (home_view, upload_view, follow_view, like_view, setting_view, profile_view, profile_info_view,
-                    delete_post_view,follow_profile_view,search_view,comments_view,post_authorinfo_view)
+from .views import (home_view, upload_view, follow_view, like_view, setting_view, profile_view,
+                    delete_post_view, follow_profile_view, search_view, comments_view,
+                    uploadprofile_view)
 
 urlpatterns = [
     path('', home_view),
@@ -9,11 +10,12 @@ urlpatterns = [
     path('like/', like_view),
     path('setting/', setting_view),
     path('profile/', profile_view),
-    path('profile_info/', profile_info_view),
+    path('profile_info/', profile_view),
     path('delete_post/', delete_post_view),
-    path('follow_profile/',follow_profile_view),
-    path('search/',search_view),
-    path('post/<int:post_id>/comments/',comments_view),
-    path('post_authorinfo/', post_authorinfo_view),
+    path('follow_profile/', follow_profile_view),
+    path('search/', search_view),
+    path('post/<int:post_id>/comments/', comments_view),
+    path('post_authorinfo/', profile_view),
+    path('upload_profile/', uploadprofile_view),
     # path('sql/', sql_test_view)
 ]

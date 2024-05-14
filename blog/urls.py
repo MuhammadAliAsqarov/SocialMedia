a9ceba7f66ae13fd02a2fcb04653b2b4cdaaba79
MukhammadAli <133798412+JavaDevALi@users.noreply.github.com>
2024-05-14 11:38:08 +0500

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (home_view, upload_view, follow_view, like_view, setting_view, profile_view,
-                    delete_post_view, follow_profile_view, search_view, comments_view,
+                    delete_post_view, search_view, comments_view,
                     uploadprofile_view)
 
 urlpatterns = [
@@ -12,7 +12,7 @@ urlpatterns = [
     path('profile/', profile_view),
     path('profile_info/', profile_view),
     path('delete_post/', delete_post_view),
-    path('follow_profile/', follow_profile_view),
+    path('follow_profile/', follow_view),
     path('search/', search_view),
     path('post/<int:post_id>/comments/', comments_view),
     path('post_authorinfo/', profile_view),

f3d862fb368f1020e87d56504cafe4d2d9685f44
MukhammadAli <133798412+MuhammadAliAsqarov@users.noreply.github.com>
2024-07-04 17:14:54 +0500

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -4,19 +4,21 @@ from django.contrib.auth.models import User
 
 class MyUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_pic = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    profile_pic = models.ImageField(upload_to='profile_pics/', default="profile_pics/person_1.jpg")
     follower_count = models.PositiveIntegerField(default=0)
-    following_count= models.PositiveIntegerField(default=0)
+    following_count = models.PositiveIntegerField(default=0)
     post_count = models.PositiveIntegerField(default=0)
-    bio= models.CharField(max_length=200, blank=True, null=True)
+    bio = models.CharField(max_length=200, default='...')
+
     def __str__(self):
         return self.user.username
+
 
 class Post(models.Model):
     author = models.ForeignKey(MyUser, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='posts/')
     like_count = models.PositiveIntegerField(default=0)
-    comment_count=models.PositiveIntegerField(default=0)
+    comment_count = models.PositiveIntegerField(default=0)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
@@ -28,12 +30,15 @@ class CommentPost(models.Model):
     author = models.ForeignKey(MyUser, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     message = models.CharField(max_length=150)
-    created_at= models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
 
 class LikePost(models.Model):
     author = models.ForeignKey(MyUser, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
 class FollowUser(models.Model):
     follower = models.ForeignKey(MyUser, related_name='follower_user', on_delete=models.CASCADE, blank=True, null=True)
     following = models.ForeignKey(MyUser, on_delete=models.CASCADE, blank=True, null=True)
